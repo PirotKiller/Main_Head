8987b40163398afef7d9c99c86270b2c55246797
PirotKiller <75603516+PirotKiller@users.noreply.github.com>
2023-09-13 20:02:29 +0530

    Initial commit

---
+++ src/main/java/tk/pirot/dev/main_head/Main_Head.java
@@ -0,0 +1,17 @@
+package tk.pirot.dev.main_head;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main_Head extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
