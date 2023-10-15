65c70332608d31db1281c541effd0fced0ebb064
PirotKiller <75603516+PirotKiller@users.noreply.github.com>
2023-10-15 17:04:32 +0530

    Ban Plugin Added

--- src/main/java/tk/pirot/dev/main_head/Main_Head.java
+++ src/main/java/tk/pirot/dev/main_head/Main_Head.java
@@ -1,12 +1,17 @@
 package tk.pirot.dev.main_head;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import tk.pirot.dev.main_head.commands.start;
+import tk.pirot.dev.main_head.listener.list;
 
 public final class Main_Head extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        
+        getCommand("bangui").setExecutor(new start());
+
+        getServer().getPluginManager().registerEvents(new list(), this);
+
 
     }
 
@@ -14,4 +19,5 @@ public final class Main_Head extends JavaPlugin {
     public void onDisable() {
         
     }
+
 }
